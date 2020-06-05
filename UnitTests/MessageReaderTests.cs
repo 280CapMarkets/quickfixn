9daa0b6cf679d204b0cc0b962d64295c06093a95
Nazar Mandzyk <nazar.mandzyk@perfectial.com>
2020-06-05 20:32:29 +0300

    - added network stream tests

--- UnitTests/MessageReaderTests.cs
+++ UnitTests/MessageReaderTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -57,6 +59,56 @@ namespace UnitTests
             Assert.AreEqual(expectedMessageCount, receivedMessageCount);
         }
 
+        [Test]
+        public async Task ReadMessages()
+        {
+            //arrange
+            var msg =
+                "8=FIXT.1.19=41935=634=675049=MA52=20200210-15:25:06.69056=280AXEOT1128=923=1054893731-0+128=R26=1054893731-0+055=[N/A]48=79642BXT522=1454=1455=US79642BXT50456=4167=NONE762=MUNI54=227=20000015=USD232=1233=MINQTY234=5000423=144=112.796640=112.7960=20200210-15:24:59236=1.0453=1448=XrefID-33447=D452=75961=MNPrice22022=120,300,900,180020120=ClientAxe-Orders20014=0.05620015=0.02829735=1054893731-0+010=019";
+
+            var receivedMessageCount = 0;
+            var expectedMessageCount = 10000;
+            var server = new Server(9212);
+            var serverSideTcpClientTask = server.WaitForClient(CancellationToken.None);
+            await Task.Delay(2000);
+            var client = new Client();
+            using var cancellationTokenSource = new CancellationTokenSource();
+            using var clientStream = client.Connect(9212);
+            using var serverStream = (await serverSideTcpClientTask).GetStream();
+
+            //act
+            var readDataFromStreamTask = _target.ReadStreamData(clientStream, cancellationTokenSource.Token);
+            var readMessageTask = _target.ReadMessages((m, ct) =>
+            {
+                receivedMessageCount++;
+                if (receivedMessageCount == expectedMessageCount)
+                {
+                    cancellationTokenSource.Cancel();
+                    clientStream.Dispose();
+                    serverStream.Dispose();
+                }
+                return Task.CompletedTask;
+            }, cancellationTokenSource.Token);
+
+            var writeDataToStreamTask = WriteChunckedMessagesToStream(msg, expectedMessageCount, serverStream, cancellationTokenSource);
+
+            try
+            {
+                await Task.WhenAll(readDataFromStreamTask, readMessageTask, writeDataToStreamTask);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                //closed stream
+            }
+            catch (TaskCanceledException ex)
+            {
+
+            }
+
+            //assert
+            Assert.AreEqual(expectedMessageCount, receivedMessageCount);
+        }
+
         private static Task WriteChunckedMessagesToStream(string msg, int count, Stream stream, CancellationTokenSource cancellationTokenSource)
         {
             var chunckedMessage = Enumerable.Range(1, msg.Length / 2).Select(i => new string(msg.Skip(i * 2).Take(2).ToArray())).ToList();
@@ -70,6 +122,43 @@ namespace UnitTests
                         streamWriter.Flush();
                     });
             }, cancellationTokenSource.Token);
+        }
+
+
+        private class Server : IDisposable
+        {
+            private readonly TcpListener _listener;
+            public Server(int port)
+            {
+                _listener = new TcpListener(new IPEndPoint(IPAddress.Any, port));
+            }
+            public Task<TcpClient> WaitForClient(CancellationToken cancellationToken)
+            {
+                return Task.Run(() =>
+                {
+                    _listener.Start();
+                    return _listener.AcceptTcpClient();
+                }, cancellationToken);
+            }
+
+            public void Dispose()
+            {
+                _listener.Server.Dispose();
+            }
+        }
+
+        private class Client
+        {
+            public Stream Connect(int port)
+            {
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                {
+                    NoDelay = true,
+                    LingerState = { Enabled = false }
+                };
+                socket.Connect(IPAddress.Parse("127.0.0.1"), port);
+                return new NetworkStream(socket, true);
+            }
         }
 
         private class ProducerConsumerStream : Stream
